9e9a8e8bbf7f258e0a069a380aed35e6e9f8c81f
Marshall Jones <marshall@balancedpayments.com>
2014-03-18 18:20:49 -0700

    fff

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,7 +17,7 @@
 #
 
 balanced_user 'andrew' do
-  sudo 
+  sudo true
   github_username 'bninja'
   ssh_keys [
     'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQClYhtXQPxx7FBOer6b9RDuXu3lKbUVs5HMJKXNbXKjQKkMeq7xHGRN0/jX0wu7ZRX6NmAjykqd4xUq6b4SXO8voL4jltbGkbqUD59WNipjlZ5ZF6TSda1cDREcqGdGMGuK6/IQujwOE45xX8WFzvSE5Xx1RI9LeGuWT9a1BC8ivhwtQNBA9mNzVDcWql8tq9LDh/xRsrqls/ul+XT5Ti0EviTdycCi3DSo2aPKYnwYm8vE5gP/lRlxSpDYP4y7WuE88PL4rCOY+3D1zBVPfRqKz+DVAyXXsoVJ1pLKBVKeiAhr3COWSkZIVr4/3IcHRzePyHDbMTUZfEZxKVh8Vnmb ai@ai-xg41',
